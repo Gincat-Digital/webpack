025b50081cc0c46f21bacffc4d22825d0057e834
Braian Rosas <axis.dev@outlook.com.ar>
2024-03-29 12:42:06 -0300

    Create client

---
+++ src/cli/client.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import webpack from 'webpack';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import path from 'path';
+
+type Argvs = {
+	config?: string;
+} & yargs.Argv['argv'];
+
+const argv: Argvs = yargs(hideBin(process.argv)).argv;
+
+if (argv.config) {
+	new Promise(async (resolve) => {
+		const configPath = path.resolve(process.cwd(), argv.config!);
+		const configURL = new URL(configPath).pathname.replace(/\\/g, '/');
+		const config = await import(configURL);
+
+		webpack(config, (err) => {
+			console.log(err);
+		});
+		resolve(null);
+	});
+}
