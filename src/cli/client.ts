2532bd00fc19e54a87b9b10477fc229671ecc4d2
Braian Rosas <axis.dev@outlook.com.ar>
2024-03-30 21:22:58 -0300

    Add try-catch

--- src/cli/client.ts
+++ src/cli/client.ts
@@ -13,47 +13,52 @@ type Argvs = {
 
 const argv: Argvs = yargs(hideBin(process.argv)).argv;
 
-if (argv.config) {
-	(async () => {
-		const configPath = path.resolve(process.cwd(), argv.config!);
-		const configURL = new URL(configPath).pathname.replace(/\\/g, '/');
-
-		const config: GincatWebpackConfig = await import(configURL).then(
-			(_config) => {
-				return _config.default || _config.config;
-			},
-		);
-
-		if (!config) {
-			throw new Error(`No webpack configuration found at: ${configURL}`);
-		}
-
-		if (config.devServer) {
-			const server = new WebpackDevServer(
-				config.devServer,
-				webpack({
-					...omit(config, ['devServer']),
-					stats: 'minimal',
-				}),
+try {
+	if (argv.config) {
+		(async () => {
+			const configPath = path.resolve(process.cwd(), argv.config!);
+			const configURL = new URL(configPath).pathname.replace(/\\/g, '/');
+	
+			const config: GincatWebpackConfig = await import(configURL).then(
+				(_config) => {
+					return _config.default || _config.config;
+				},
 			);
-			server.start();
-		} else {
-			webpack(config, (error, stats) => {
-				if (error) {
-					throw new Error(error.message);
-				} else {
-					if (stats) {
-						console.log(
-							stats.toString({
-								preset: 'minimal',
-								colors: true,
-							}),
-						);
+	
+			if (!config) {
+				throw new Error(`No webpack configuration found at: ${configURL}`);
+			}
+	
+			if (config.devServer) {
+				const server = new WebpackDevServer(
+					config.devServer,
+					webpack({
+						...omit(config, ['devServer']),
+						stats: 'minimal',
+					}),
+				);
+				server.start();
+			} else {
+				webpack(config, (error, stats) => {
+					if (error) {
+						throw new Error(error.message);
+					} else {
+						if (stats) {
+							console.log(
+								stats.toString({
+									preset: 'minimal',
+									colors: true,
+								}),
+							);
+						}
 					}
-				}
-			});
-		}
-	})();
-} else {
-	throw new Error(`No config path was found in client options, please provide '--config' flag with its respective config file ubication. For example: 'gcwp --config=path/to/webpack.config.js'`);
+				});
+			}
+		})();
+	} else {
+		throw new Error(`No config path was found in client options, please provide '--config' flag with its respective config file ubication. For example: 'gcwp --config=path/to/webpack.config.js'`);
+	}
+} catch (error) {
+	console.log(error);
 }
+
